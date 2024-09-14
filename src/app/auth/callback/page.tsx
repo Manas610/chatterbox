7b9429eb59abcd8ee23c6070604ab27ecdc91dd0
Manas <singhmanas610@gmail.com>
2024-09-14 22:00:07 +0530

    deployment fix 3

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 import { Loader } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 
-const Page = () => {
+const CallbackPage = () => {
 
   const router = useRouter()
   const {data} = useQuery({
@@ -27,4 +27,4 @@ const Page = () => {
   )
 }
 
-export default Page
+export default CallbackPage
